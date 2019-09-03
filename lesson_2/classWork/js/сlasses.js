2d44122af547eb05a18f85bcf9e861ca6ad407cd
NastiaWEB <gatsenko.nastja@gmail.com>
2019-09-03 22:44:08 +0300

    Add advertisiment

--- lesson_2/classWork/js/сlasses.js
+++ lesson_2/classWork/js/сlasses.js
@@ -1,3 +1,46 @@
+const data = [
+  {
+    id: 1,
+    link: "#1",
+    name: "Established fact123123",
+    description: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
+    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400926_file_162303.jpg",
+    likesCount: 10
+  },
+  {
+    id: 2,
+    link: "#1",
+    name: "Established fact",
+    description: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
+    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400926_file_162303.jpg",
+    likesCount: 0
+  },
+  {
+    id: 3,
+    link: "#3",
+    name: "Suffered alteration",
+    description: "Looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature.",
+    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400896_file_162315.jpg",
+    likesCount: 3
+  },{
+    id: 4,
+    link: "#4",
+    name: "Discovered source",
+    description: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400878_file_162324.jpg",
+    likesCount: 5,
+  },{
+    id: 5,
+    link: "#5",
+    name: "Handful model",
+    description: "The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400876_file_162328.jpg",
+    likesCount: 36
+  },
+];
+
+advData = [{id: 671, img:"https://news.rambler.ru/img/2018/10/13224654.555620.1244.jpeg"}, {id:672, img: "https://gdetraffic.com/img/news_2283/tp/710x293__t.jpg"}];
+
 /*
   Задание:
 
@@ -22,19 +65,6 @@
 
     III.  Сгенерировать ленту из всех постов что вы добавили в систему.
           Каждый третий пост должен быть рекламным
-
-    <div class="post">
-      <div class="post__title">Some post</div>
-      <div class="post__image">
-        <img src="https://cdn-images-1.medium.com/fit/t/1600/480/1*Se0gnNo-tsQG-1jeu_4TJw.png"/>
-      </div>
-      <div class="post__description"></div>
-      <div class="post__footer">
-        <button class="post__like">Like!</button>
-      </div>
-    </div>
-
-
 
 
 */
@@ -94,52 +124,25 @@ class Post {
   }
 };
 
-class Advertisment extends Post {
-  constructor() {
-    super()
+function newAdvertisment({id, img}) {
+    return `<div class="adv__image" id="${id}"><img src="${img}"/> <button onClick="alert('This product ${id} added to card!')"> Add to card </button> </div>`;
+}
+function displayAdv(quantity, item){
+  let selectedChild;
+  for(let i = 0; i < quantity; i++){
+    selectedChild =  document.getElementById("posts_feed").children[2+i];
+    console.log(selectedChild);
   }
+  selectedChild.insertAdjacentHTML("afterEnd", item)
 }
 
-const data = [
-  {
-    id: 1,
-    link: "#1",
-    name: "Established fact123123",
-    description: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
-    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400926_file_162303.jpg",
-    likesCount: 10
-  },
-  {
-    id: 2,
-    link: "#1",
-    name: "Established fact",
-    description: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
-    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400926_file_162303.jpg",
-    likesCount: 0
-  },
-  {
-    id: 3,
-    link: "#3",
-    name: "Suffered alteration",
-    description: "Looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature.",
-    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400896_file_162315.jpg",
-    likesCount: 3
-  },{
-    id: 4,
-    link: "#4",
-    name: "Discovered source",
-    description: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
-    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400878_file_162324.jpg",
-    likesCount: 5,
-  },{
-    id: 5,
-    link: "#5",
-    name: "Handful model",
-    description: "The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-    image: "http://telegram.org.ru/uploads/posts/2017-10/1507400876_file_162328.jpg",
-    likesCount: 36
-  },
-];
+function addAdvertisment() {
+  let newAdv;
+  advData.map(adv => {
+    newAdv = newAdvertisment(adv);
+  })
+  displayAdv(advData.length, newAdv);
+}
 
 function renderPostList() {
   let newPost;
@@ -147,54 +150,23 @@ function renderPostList() {
   data.map(data => {
     newPost = new Post(data);
     newPost.render();
-  })
+  });
+
+  addAdvertisment();
 }
 
 
-// function Card ({id, link, name, description, image, likesCount, like = false, comments = []})  {
-//   this.id = id;
-//   this.link = link;
-//   this.name = name;
-//   this.description = description;
-//   this.image = image;
-//   this.like = like;
-//   this.likesCount = likesCount;
+// class Advertisment {
+//   constructor({id, img}) {
+//     this.id = id;
+//     this.img = img;
+//   }
+//   postTemplate() {
+//     return `<div class="adv__image" id="${this.id}"><img src="${this.img}"/> <button onClick="alert('This product added to card!')"> Add to card </button> </div>`;
+//   }
+//   displayAdv(){
+//     const selectedPost = document.getElementById("posts_feed");
+//     selectedPost.insertAdjacentHTML("beforeEnd", this.postTemplate())
+//   }
 //
-//   this.onLikeChange = () => {
-//     this.like = !this.like;
-//     if(this.like){ this.likesCount += 1}
-//     else if(!this.like && this.likesCount !== likesCount){ this.likesCount -= 1}
-//     console.log(this.likesCount);
-//    };
-//
-//   this.displayPost = () => {
-//     const {id, link, name, description, image, like, likesCount, comments} = this;
-//     return (`<div id=${id} class="post-wrapper">
-//                 <img src=${image} alt="image" class="post-img"/>
-//                 <div class="post-content-wrapper">
-//                     <a href=${link} class="post-link" >${name}</a>
-//                     <p class="post-content">${description}</p>
-//                     <img src=${like ? "liked.svg": "heart.svg"} class="icon" "/>
-//                     <span>${likesCount}</span>
-//                 <div>
-//             </div>`);
-//     };
-// };
-//
-// function createNewItem(item) {
-//   const post = new Card(item);
-//   let listItem = document.createElement('li');
-//   listItem.innerHTML = post.displayPost();
-//   listItem.querySelector(".icon").addEventListener('click', post.onLikeChange);
-//   return listItem;
-// };
-//
-// function displayPostPage() {
-//   let posts = document.createElement('ul');
-//   posts.className = "posts-list"
-//   data.map(item => {
-//     const newItem = createNewItem(item);
-//     posts.appendChild(newItem);
-//   });
-//   renderArea.appendChild(posts);
-// };
+// }
